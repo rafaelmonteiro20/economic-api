4a0b53f28fdf463a012ec0aef99c717f01493805
Rafael Monteiro <rafajapones@gmail.com>
2017-07-20 20:29:48 -0300

    Buscando categoria pelo código.

--- src/main/java/com/economic/resource/CategoriasResource.java
+++ src/main/java/com/economic/resource/CategoriasResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,6 +38,10 @@ public class CategoriasResource {
 		return ResponseEntity.created(uri).body(categoria);
 	}
 	
-	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> buscarPorId(@PathVariable Long id) {
+		Categoria categoria = repository.findOne(id);
+		return categoria == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(categoria);
+	}
 	
 }
