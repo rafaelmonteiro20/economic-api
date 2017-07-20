2cf3581c689b4c98cd793519d1532ecf7fdc73b0
Rafael Monteiro <rafajapones@gmail.com>
2017-07-20 20:25:44 -0300

    Criando novo recurso categoria

--- src/main/java/com/economic/resource/CategoriasResource.java
+++ src/main/java/com/economic/resource/CategoriasResource.java
@@ -1,11 +1,16 @@
 package com.economic.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.economic.model.Categoria;
 import com.economic.repository.CategoriaRepository;
@@ -21,5 +26,17 @@ public class CategoriasResource {
 	public List<Categoria> listar() {
 		return repository.findAll();
 	}
+	
+	@PostMapping
+	public ResponseEntity<?> criar(@RequestBody Categoria categoria) {
+		categoria = repository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+											 .buildAndExpand(categoria.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(categoria);
+	}
+	
+	
 	
 }
