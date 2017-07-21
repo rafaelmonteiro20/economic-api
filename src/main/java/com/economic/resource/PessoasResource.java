1787b47d1800d780aeb056c3e31dfea03fc65a49
Rafael Monteiro <rafajapones@gmail.com>
2017-07-20 21:37:32 -0300

    Criando novo recurso pessoa

--- src/main/java/com/economic/resource/PessoasResource.java
+++ src/main/java/com/economic/resource/PessoasResource.java
@@ -1,11 +1,17 @@
 package com.economic.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.economic.model.Pessoa;
 import com.economic.repository.PessoaRepository;
@@ -20,6 +26,22 @@ public class PessoasResource {
 	@GetMapping
 	public List<Pessoa> listar() {
 		return pessoaRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> criar(@RequestBody Pessoa pessoa) {
+		pessoa = pessoaRepository.save(pessoa);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+								   .buildAndExpand(pessoa.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(pessoa);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?> buscarPorId(@PathVariable Long id) {
+		Pessoa pessoa = pessoaRepository.findOne(id);
+		return pessoa == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(pessoa);
 	}
 	
 }
