9518145f63506209e806bbce630b1bae4ed0bc2b
Rafael Monteiro <rafajapones@gmail.com>
2017-07-24 20:00:37 -0300

    Removendo o recurso pessoa

--- src/main/java/com/economic/resource/PessoasResource.java
+++ src/main/java/com/economic/resource/PessoasResource.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.economic.event.RecursoCriadoEvent;
@@ -47,6 +49,12 @@ public class PessoasResource {
 	public ResponseEntity<?> buscarPorId(@PathVariable Long id) {
 		Pessoa pessoa = pessoaRepository.findOne(id);
 		return pessoa == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(pessoa);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long id) {
+		pessoaRepository.delete(id);
 	}
 	
 }
